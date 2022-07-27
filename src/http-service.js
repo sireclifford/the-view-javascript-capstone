825465018a72257f9cf0c968c9bab8235933cb32
Anas Abubakar <anasabubakar2015@gmail.com>
2022-07-27 16:49:16 +0100

    Add the code for the comment section

--- src/http-service.js
+++ src/http-service.js
@@ -1,7 +1,9 @@
 const baseUrl = 'https://yts.mx/api/v2';
 const listMoviesAPI = '/list_movies.json';
+const singleMovieAPI = '/movie_details.json';
 
 module.exports = {
   baseUrl,
   listMoviesAPI,
+  singleMovieAPI,
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import logo from './assets/images/the-view-logo.png';
 import like from './assets/images/like.png';
 
 import fetchMovies from './modules/fetchMovies';
+import createComment from './modules/renderComment';
+import fetchComment from './modules/fetchComment';
 
 const displayMoviesUI = (movies) => {
   const moviesList = document.querySelector('.main');
@@ -12,18 +14,28 @@ const displayMoviesUI = (movies) => {
     const movieEl = document.createElement('li');
     movieEl.classList.add('movie-item');
     movieEl.innerHTML = `
-       <div class="movie-item" id="${movie.id}">
+        <div class="movie-item" id="${movie.id}">
             <img class="flyer" src="${movie.medium_cover_image}" alt="gray_man">
             <div class="movie-footer">
                 <div class="title">
                     <p>${movie.title_long}</p>
                     <img class="like-icon" src="${like}" alt="like_icon">
                 </div>
-                <button type="submit" class="comment-btn">Comments</button>
+                <button id="${movie.imdb_code} "type="submit" class="comment-btn">Comments</button>
             </div>
         </div>
         `;
     moviesList.appendChild(movieEl);
+  });
+  const commentButton = document.querySelectorAll('.comment-btn');
+  const commentPopup = document.querySelector('.popup');
+  commentButton.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      fetchComment(e.target.id).then((response) => {
+        createComment(response.data.data.movie);
+        commentPopup.style.display = 'block';
+      });
+    });
   });
 };
 
