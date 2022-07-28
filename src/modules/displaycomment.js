c9cddb787e3ea197e01253230e0d00078ce48ff3
Anas Abubakar <anasabubakar2015@gmail.com>
2022-07-28 16:08:46 +0100

    Add the comment section

---
+++ src/modules/displaycomment.js
@@ -0,0 +1,14 @@
+// const displayCommments = async (movieId) => {
+  
+//   try {
+//     const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/6MIoDJ3ru05goBGOD/comments?item_id=${itemId}`);
+//     const data = await response.json();
+//     console.log(data);
+//     return data; 
+//   } catch (error) {
+//     console.log(error);
+//   }
+    
+//   };
+
+// export default displayCommments;
--- src/modules/renderComment.js
+++ src/modules/renderComment.js
@@ -1,10 +1,13 @@
 /* eslint-disable*/
+import createComments from "./createComment";
+import displayCommments from "./displaycomment";
+
 const popupSection = document.querySelector('.popup');
 const createComment = async (movie) => {
   const comment = document.createElement('div');
   comment.classList.add('comment-container');
   comment.innerHTML = `
-  <div class="movie-comment" id="">
+  <div class="movie-comment" id="${movie.id}">
     <i class="fa-solid fa-xmark"></i>
     <div>
       <img class="flyer" src="${movie.large_cover_image}" alt="gray_man">
@@ -20,6 +23,36 @@ const createComment = async (movie) => {
         <p>Downloads: ${movie.download_count}</p>
       </div>
     </div>
+    <div class="comment-section">
+      <h3>Comments</h3>
+      <ul class="comments">
+        <li>
+        <div class="comment-item">
+        <p>name: </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+      </div>
+        </li>
+        <li>
+        <div class="comment-item">
+        <p>name: </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+      </div>
+        </li>
+        <li>
+        <div class="comment-item">
+        <p>name: </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+      </div>
+        </li>
+
+      </ul>
+      <form class="comments-form">
+      <h3>Add a comment</h3>
+      <input class="border" type=text placeholder="Your name" id="username" name="usernmae">
+      <textarea class="border" placeholder="Your comment" id="comment" name="comment"></textarea>
+      <button class="border comment-btn" type="submit">Submit</button>   
+      </form>
+    </div>
   </div>
   `;
   popupSection.appendChild(comment);
@@ -29,6 +62,40 @@ const createComment = async (movie) => {
     commentPopup.style.display = 'none';
     location.reload();
   });
+  const showComment = async (id) => {
+    const comments = await displayCommments(id);
+    console.log(comments);
+    const commentList = document.querySelector('.comments');
+    comments.forEach((comment) => {
+      const commentItem = document.createElement('li');
+      commentItem.innerHTML = `
+      <div class="comment-item">
+        <p>${comment.username}</p>
+        <p>${comment.comment}</p>
+      </div>
+      `;
+      commentList.appendChild(commentItem);
+    }
+    );
+  }
+
+  const inputName = document.querySelector('#username').value;
+  const inputComment = document.querySelector('#comment').value;
+  const form = document.querySelector('.comments-form');
+  form.addEventListener('submit', (e) => {
+    console.log(inputName)
+    e.preventDefault();
+    createComments({
+      "item_id": movie.id,
+      "username": inputName,
+      "comment": inputComment,
+  }).then((res) => {
+    console.log(res);
+  })
+
+
+  })
+  showComment(movie.id);
 };
 
 export { createComment as default };
