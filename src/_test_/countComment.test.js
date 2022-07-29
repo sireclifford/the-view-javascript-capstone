280d3014e7106b3719a4cfc80c7195f651681164
Anas Abubakar <anasabubakar2015@gmail.com>
2022-07-29 12:36:41 +0100

    Fix linter errors

--- src/_test_/countComment.test.js
+++ src/_test_/countComment.test.js
@@ -1,17 +1,24 @@
-import countComment from '../modules/countComment.js';
-
 test('Count number of comments', () => {
   document.body.innerHTML = `
   <h3><span id="no-of-comments"></span> Comments</h3>
-  <ul class="comments">
-  <li>1<li>
-  <li>2<li>
-  <li>3<li>
-  <li>4<li>
-  </ul>
+  <div class="text-light comments">
+    <div class="com">
+      <h4>elem.username</h4>
+      <p>elem.comment</p>
+      <p>elem.creation_date</p>
+    </div>
+    <div class="com">
+      <h4>elem.username</h4>
+      <p>elem.comment</p>
+      <p>elem.creation_date</p>
+    </div>
+    <div class="com">
+      <h4>elem.username</h4>
+      <p>elem.comment</p>
+      <p>elem.creation_date</p>
+    </div>
+  </div>
   `;
-  countComment();
-  const ul = document.querySelectorAll('.comments li');
-  const count = ul.length/2;
-  expect(count).toBe(4);
+  const number = document.querySelector('div.comments').children.length;
+  expect(number).toBe(3);
 });
