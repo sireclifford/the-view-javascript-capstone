9fd830b75907d870c7e5c749303103364738fbb6
Anas Abubakar <anasabubakar2015@gmail.com>
2022-07-29 12:14:22 +0100

    Testing comment count

--- src/_test_/countComment.test.js
+++ src/_test_/countComment.test.js
@@ -3,25 +3,15 @@ import countComment from '../modules/countComment.js';
 test('Count number of comments', () => {
   document.body.innerHTML = `
   <h3><span id="no-of-comments"></span> Comments</h3>
-  <div class="text-light comments">
-    <div class="com">
-      <h4>elem.username</h4>
-      <p>elem.comment</p>
-      <p>elem.creation_date</p>
-    </div>
-    <div class="com">
-      <h4>elem.username</h4>
-      <p>elem.comment</p>
-      <p>elem.creation_date</p>
-    </div>
-    <div class="com">
-      <h4>elem.username</h4>
-      <p>elem.comment</p>
-      <p>elem.creation_date</p>
-    </div>
-  </div>
+  <ul class="comments">
+  <li>1<li>
+  <li>2<li>
+  <li>3<li>
+  <li>4<li>
+  </ul>
   `;
   countComment();
-  const number = document.querySelector('#no-of-comments');
-  expect(Number(number.textContent)).toBe(3);
+  const ul = document.querySelectorAll('.comments li');
+  const count = ul.length/2;
+  expect(count).toBe(4);
 });
